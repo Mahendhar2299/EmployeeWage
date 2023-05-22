f708866ba552dddabbc0e774440c7d4f479111ba
Mahendhar2299 <delphic22mahi@gmail.com>
2023-05-22 19:11:27 +0530

    added: wage per month

--- src/com/bridgelabz/EmpWage.java
+++ src/com/bridgelabz/EmpWage.java
@@ -5,13 +5,17 @@ public class EmpWage {
     public static final int IS_FULL_TIME = 2;
     public static final int EMP_RATE_PER_HOUR = 20;
     public static final int NUM_OF_WORKING_DAYS = 20;
+    public static final int MAX_HRS_IN_MONTH = 100;
 
     public static void main(String[] args) {
 
         System.out.println("Welcome to Employee Wage Computation Program ");
 
-        int empHrs = 0, empWage = 0, totalEmpWage = 0;
-        for (int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+        int totalEmpWage = 0, totalWorkingDays = 0, totalEmpHrs = 0;
+        while (totalEmpHrs <= MAX_HRS_IN_MONTH &&
+                totalWorkingDays < NUM_OF_WORKING_DAYS) {
+            int empHrs = 0;
+            totalWorkingDays++;
             double empCheck = Math.floor(Math.random() * 10) % 3;
             switch ((int) empCheck) {
                 case IS_FULL_TIME:
@@ -24,7 +28,8 @@ public class EmpWage {
                     empHrs = 0;
                     break;
             }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
+            totalEmpHrs += empHrs;
+            int empWage = empHrs * EMP_RATE_PER_HOUR;
             totalEmpWage += empWage;
             System.out.println("Emp Wage: " + empWage);
         }
